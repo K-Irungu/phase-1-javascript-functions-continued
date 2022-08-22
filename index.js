1afacad70330cf1499c6d06754d7e6fbc4566dab
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-08-22 18:56:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity2 = "go to the office") {
+    return `This Monday, I will ${activity2}.`;
+}
+
+function wrapAdjective(flairParam = "*") {
+    return function(singleParam = "special") {
+        return `You are ${flairParam}${singleParam}${flairParam}!`;
+    }
+}
+console.log(wrapAdjective("%")("a dedicated programmer"));
+
